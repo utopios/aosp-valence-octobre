0f3ab44d9711550f23210719f988d65d9e44f528
ihab abadi <ihab@utopios.net>
2023-10-17 12:05:33 +0200

    check permission

--- utilisation-service-app/app/src/main/java/com/example/serviceapp/service/MathService.java
+++ utilisation-service-app/app/src/main/java/com/example/serviceapp/service/MathService.java
@@ -2,6 +2,7 @@ package com.example.serviceapp.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.os.RemoteException;
 
@@ -17,6 +18,9 @@ public class MathService extends Service {
 
         @Override
         public int addition(int a, int b) throws RemoteException {
+           if(checkCallingOrSelfPermission("com.example.serviceapp.service.PERMISSION_ACCESS_SERVICE") != PackageManager.PERMISSION_GRANTED) {
+                throw new SecurityException("Permission not granted to access service");
+            }
             return a + b;
         }
     };
